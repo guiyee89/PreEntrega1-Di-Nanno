5b2626194d9d721ea0cf296981965794281ad761
guiyee <guillermodinanno@gmail.com>
2022-12-28 23:23:23 -0300

    probando otra funcion, quizas de mas

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ let menu
 do {
     menu = prompt(" 1- Purchase your Travel Plan \n 2- Go to Website")
     menu = parseInt(menu)
+
     switch (menu) {
         case 1: comprarViaje()
             break
@@ -16,6 +17,7 @@ function comprarViaje() {
         alert("Select your destination")
         let destino = prompt("1- Sydney \n 2- Melbourne \n 3- Brisbane \n 4- Gold Coast");
         destino = parseInt(destino);
+
         switch (destino) {
             case 1: comprarServicios()
                 break
@@ -30,13 +32,27 @@ function comprarViaje() {
     } while (destino <= 1 || destino >= 4)
 
     function comprarServicios() {
-        let servicios
-        alert("Select different services");
-        servicios = prompt("1- Luggage \n 2- Insurance \n 3- Travel Planning \n 4- Customer Service");
-        servicios = parseInt(servicios);
+        alert("Select one or more services")
+        do {
+            let servicios
+            servicios = prompt("1- Luggage \n 2- Insurance \n 3- Travel Planning \n 4- Customer Service \n\n 5- Done!");
+            servicios = parseInt(servicios);
+            switch (servicios) {
+                case 1: comprarServicios()   
+                    break
+                case 2: comprarServicios()
+                    break
+                case 3: comprarServicios()
+                    break
+                case 4: comprarServicios()
+                    break
+                case 5: alert("Thanks for your purchase!")
+                    break
+                default: alert("Invalid Option")
+            }
+        } while (servicios <= 1 || servicios >= 5)
     }
 }
-
 
 
 
