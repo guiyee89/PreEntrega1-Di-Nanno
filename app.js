9434a6d3ce5da0a97f0e2fd35b5e8ef55fb4c951
guiyee <guillermodinanno@gmail.com>
2023-01-01 15:13:47 -0300

    arreglo de ciclos y calculadora

--- app.js
+++ app.js
@@ -1,56 +1,59 @@
-let menu
-do {
-    menu = prompt(" 1- Purchase your Travel Plan \n 2- Go to Website")
-    menu = parseInt(menu)
 
-    switch (menu) {
-        case 1: comprarViaje()
-            break
-        case 2: alert("Bye!")
-            break
-        default: alert("Invalid Option")
-    }
-} while (menu != 2);
-
-function comprarViaje() {
+function menuPrincipal() {
+    let total = 0;
+    let menu;
     do {
-        alert("Select your destination for $1000")
-        let destino = prompt("1- Sydney \n 2- Melbourne \n 3- Brisbane \n 4- Gold Coast");
-        destino = parseInt(destino);
-
-        switch (destino) {
-            case 1: comprarServicios()
-                break
-            case 2: comprarServicios()
-                break
-            case 3: comprarServicios()
-                break
-            case 4: comprarServicios()
-                break
+        menu = prompt(" Menu\n\n 1- Purchase a Travel Plan \n\n 2- Calculate Total Amount \n\n 3- Go to Website")
+        menu = parseInt(menu)
+        switch (menu) {
+            case 1: total = total + comprarDestino()
+                break;
+            case 2: alert("Total amount for your plan is " + total)
+                total = 0
+                break;
+            case 3: ("Bye")
+                break;
             default: alert("Invalid Option")
         }
-    } while (destino <= 1 || destino >= 4)
-
-    function comprarServicios() {
-        alert("Select one or more services")
-        do {
-            let servicios = 0
-            servicios = prompt(" 1- $75 Luggage \n 2- $100 Insurance \n 3- $150 Travel Planning \n 4- $150 Customer Service \n\n 5- Done!");
-            servicios = parseInt(servicios);
-            switch (servicios) {
-                case 1: comprarServicios()
-                    break
-                case 2: comprarServicios()
-                    break
-                case 3: comprarServicios()
-                    break
-                case 4: comprarServicios()
-                    break
-                case 5: alert("Thanks for your purchase!")
-                    break
-                default: alert("Invalid Option")
-            }
-
-        } while (servicios <= 1 || servicios >= 5)
-    }
+    } while (menu != 3)
 }
+
+function comprarDestino() {
+    let destino = 0;
+    let menuDestino
+    alert("Select your destination")
+    menuDestino = prompt(" 1- Sydney $1100 \n 2- Melbourne $1000")
+    menuDestino = parseInt(menuDestino)
+
+    switch (menuDestino) {
+        case 1: destino = 1100 + comprarServicio()
+            break
+        case 2: destino = 1000 + comprarServicio()
+            break
+        default: ("Invalid Option")
+    }
+    return destino;
+}
+
+function comprarServicio() {
+    let servicio = 0;
+    let menuServicio
+    do {
+        alert("Select different services")
+        menuServicio = prompt(" 1- Luggage $75 \n 2- Insurance $100 \n\n 3- Done and back to Menu")
+        menuServicio = parseInt(menuServicio)
+
+        switch (menuServicio) {
+            case 1: servicio = servicio + 75
+                break
+            case 2: servicio = servicio + 100
+                break
+            case 3: return servicio
+                break
+            default: ("Invalid Option")
+        }
+    } while (menuServicio <= 1 || menuServicio >= 2)
+}
+
+menuPrincipal()
+
