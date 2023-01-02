005c6e08be30ef06aa3d611d0db72d088a24e54a
guiyee <guillermodinanno@gmail.com>
2023-01-02 16:27:33 -0300

    cambio de calc total

--- app.js
+++ app.js
@@ -1,74 +1,87 @@
-const TOTAL = 0
+let total = 0;
+
 function menuPrincipal() {
     let menu;
+
     do {
-        menu = prompt(" Menu\n\n 1- Purchase a Travel Plan \n\n 2- Calculate Total Amount \n\n 3- Go to Website")
+        menu = prompt(" Menu\n\n 1- Comprar pack de viaje \n 2- Calcular total \n\n 3- Ir a sitio web")
         menu = parseInt(menu)
         switch (menu) {
-            case 1: TOTAL = TOTAL + comprarDestino()
+            case 1: comprarDestino()
                 break;
-            case 2: alert("Total amount for your plan is " + total)
+            case 2: alert("El monto total de tu Pack de Viajes es " + total)
                 total = 0
                 break;
-            case 3: ("Bye")
+            case 3: ("Nos vemos!")
                 break;
-            default: alert("Invalid Option")
+            default: alert("Opcion Invalida")
         }
-    } while (menu != 3)
+    } 
+    while (menu != 3)
 }
 
-function comprarDestino() {
-    let destino = 0;
-    let menuDestino
-    alert("Select your destination")
-    menuDestino = prompt(" 1- Sydney $1100 \n 2- Melbourne $1000")
-    menuDestino = parseInt(menuDestino)
 
-    switch (menuDestino) {
-        case 1: destino = 1100 + comprarServicio()
-            break
-        case 2: destino = 1000 + comprarServicio()
-            break
-        default: ("Invalid Option")
-    }
-    return destino;
+function comprarDestino() {
+    let menuDestino
+    /* do { */
+        alert("Selecciona tu destino")
+        menuDestino = prompt(" 1- Sydney $1200 \n 2- Melbourne $1100 \n 3- Brisbane $1000 \n 4- Gold Coast $1000 \n\n 5- Volver al menu")
+        menuDestino = parseInt(menuDestino)
+
+        switch (menuDestino) {
+            case 1: total += 1200            
+            comprarServicio()
+                break
+            case 2: total += 1100           
+            comprarServicio()
+                break
+            case 3: total += 1000            
+            comprarServicio()
+                break
+            case 4: total += 1000            
+            comprarServicio()
+                break
+            case 5: menuPrincipal()
+                break
+            default: alert("Opcion Invalida")                 
+        }
+    /* } while (menuDestino != 5) */
+    return total;
 }
 
 function comprarServicio() {
-    let servicio = 0;
     let menuServicio
+    alert("Selecciona diferentes servicios")
+
     do {
-        alert("Select different services")
-        menuServicio = prompt(" 1- Luggage $75 \n 2- Insurance $100 \n\n 3- Done and back to Menu")
+        menuServicio = prompt(" 1- Equipaje $75 \n 2- Seguro de viaje $100 \n 3- Atencion al cliente $100 \n 4- Planificador de viajes $125 \n\n 5- Calcular total")
         menuServicio = parseInt(menuServicio)
 
         switch (menuServicio) {
-            case 1: servicio = servicio + 75
+            case 1: total += 75
+                alert("Podes elegir mas")
                 break
-            case 2: servicio = servicio + 100
+            case 2: total += 100
+                alert("Podes elegir mas")
                 break
-            case 3: servicio = servicio + totalCuenta()
+            case 3: total += 100
+                alert("Podes elegir mas")
                 break
-            default: ("Invalid Option")
+            case 4: total += 125
+                alert("Podes elegir mas")
+                break
+            case 5: alert("Podes seguir comprando o calcular el total " + total) 
+                    return total
+                break
+            default: alert("Opcion Invalida")
         }
-    } while (menuServicio <= 1 || menuServicio >= 2)
-
-    return servicio;
+    } 
+    while (menuServicio != 5)
+    return total
 }
 
-function totalCuenta(){
-    let totalPack = TOTAL
-    let menuTotal
-    menuTotal = prompt(" 1- Calculate total amount \n\n 2- Go back to Menu")
-    menuTotal = parseInt(menuTotal)
-
-    switch(menuTotal){
-        case 1: alert("Total " + totalPack)
-        break
-        case 2: menuPrincipal()
-        break
-        default: alert("Invalid Option")
-    }
+function calcTotal(){
+    alert("El total de la cuenta es " + TOTAL)
 }
 
 menuPrincipal()
